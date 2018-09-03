72212eda0247afd59a18b76379964f976762e79b
Lyn Muldrow <lmuldrow@linkedin.com>
2018-09-03 10:10:20 -0700

    started AppHeader component

---
+++ they-did-that/src/components/Appheader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+} from '@material-ui';
+
+const AppHeader = () => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="title" color="inherit">
+        They Did That
+      </Typography>
+    </Toolbar>
+  <AppBar>
+);
+
+export default AppHeader;
