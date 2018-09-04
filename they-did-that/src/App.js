cb756527c9fab436ccb0f595a4994a4b1584cbab
Lyn Muldrow <lmuldrow@linkedin.com>
2018-09-03 22:25:52 -0700

    edited app.js to include implicit callback

--- they-did-that/src/App.js
+++ they-did-that/src/App.js
@@ -1,29 +1,23 @@
 import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+import { ImplicitCallback } from '@okta/okta-react';
 import {
   CssBaseline,
   withStyles,
 } from '@material-ui/core';
 
-import AppHeader  from './components/Appheader.js';
+import AppHeader from './components/Appheader.js';
 import Home from './pages/Home.js';
-
-const styles = theme => ({
-  main: {
-    padding: 3 * theme.spacing.unit,
-    [theme.breakpoints.down('xs')]: {
-      padding: 2 * theme.spacing.unit,
-    },
-  },
-});
 
 const App = ({ classes }) => (
   <Fragment>
     <CssBaseline />
     <AppHeader />
     <main className={classes.main}>
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/implicit/callback" component={ImplicitCallback} />
     </main>
   </Fragment>
 );
 
-export default withStyles(styles)(App);
+export default withStyles()(App);
