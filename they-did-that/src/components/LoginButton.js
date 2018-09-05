e40856a2d5f2ec1e446d68d6bea5084a8051a3bc
Lyn Muldrow <lmuldrow@linkedin.com>
2018-09-05 10:25:52 -0700

    added notes on LoginButton

--- they-did-that/src/components/LoginButton.js
+++ they-did-that/src/components/LoginButton.js
@@ -1,3 +1,4 @@
+//Import all necessary dependencies from different libraries.
 import React, { Component } from 'react';
 import {
   Button,
@@ -9,6 +10,7 @@ import {
 import { AccountCircle } from '@material-ui/icons';
 import { withAuth } from '@okta/okta-react';
 
+//Create a component, LoginButton, that starts with an initial blank state.
 class LoginButton extends Component {
   state = {
     authenticated: null,
@@ -16,6 +18,7 @@ class LoginButton extends Component {
     menuAnchorEl: null,
   };
 
+//Check to see if the user is authenticated on first mount- if so, initialize or update the component.
   componentDidUpdate() {
     this.checkAuthentication();
   }
@@ -24,6 +27,7 @@ class LoginButton extends Component {
     this.checkAuthentication();
   }
 
+//On mount, use the auth prop to check to see if the user signed in. If so, set the state to authenticated.
   async checkAuthentication() {
     const authenticated = await this.props.auth.isAuthenticated();
     if (authenticated !== this.state.authenticated) {
@@ -32,15 +36,18 @@ class LoginButton extends Component {
     }
   }
 
+//Define login and logout actions.
   login = () => this.props.auth.login();
   logout = () => {
     this.handleMenuClose();
     this.props.auth.logout();
   };
 
+//Define what happens when the menu is opened and closed, then set show the appropriate view, placement, and state accordingly.
   handleMenuOpen = event => this.setState({ menuAnchorEl: event.currentTarget });
   handleMenuClose = () => this.setState({ menuAnchorEl: null });
 
+//This is what will be shown on the screen. If the user is not authenticated, it will show the login button and start the auth process.
   render() {
     const { authenticated, user, menuAnchorEl } = this.state;
 
@@ -76,4 +83,5 @@ class LoginButton extends Component {
   }
 }
 
+//Use the withAuth function to wrap the component and return the authenticated user view.
 export default withAuth(LoginButton);
